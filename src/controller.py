9d9e0d47af3d6c02cc0211335be4c7c9cc17f362
robrecht.peeters <robrecht.peeters@student.kuleuven.be>
2021-02-17 17:00:35 +0100

    second commint

--- src/controller.py
+++ src/controller.py
@@ -17,13 +17,14 @@ class Controller:
         nactions: aantal actions
     """
 
-    def __init__(self, pomdp, sim, weight, exploration):
+    def __init__(self, pomdp, sim, weight, exploration, logger):
         self.pomdp = pomdp
         self.sim = sim
         self.weight = weight
         self.hist = []
         self.exp = exploration
         self.nactions = len(pomdp.pomdpenv.actions) #todo: logger meegeven die gecalled wordt bij elke method call
+        self.logger = logger
 
     def step(self):
         belief = self.pomdp.belief
@@ -34,10 +35,55 @@ class Controller:
         obs = self.sim.step(int(action_num))
         self.pomdp.update_belief(int(action_num), obs)
         self.hist.append((belief, int(action_num), obs))
+        #todo: log
 
     def update(self):
-        #calc most likely transition function given history
-        print("todo update")
+        '''
+        calc most likely transition function t given history
+        t = nparray met elke entry: (a, s, s')
+        '''
+        nstates = len(self.pomdp.pomdpenv.states)
+        t = np.zeros((self.nactions, nstates, nstates))
+        w = 1
+        b = self.pomdp.belief
+        for step in reversed(self.hist):
+            counts = np.outer(step[0], b) #calc outer product of b(s) and b(s') (product van elke combinatie)
+            t[step[1]] += w*counts        #add these counts to the corresponding action
+            b = step[0]                   #set b(s') to current for next iter
+            w = w*self.weight             #decrease weight
+
+        #normalize
+        for i in range(len(t)):
+            for j in range(len(t[i])):
+                if t[i][j].sum() == 0: #if unseen set all to one to normalize to random
+                    t[i][j] = np.ones(len(t[i][j]))
+                t[i][j] = t[i][j]/t[i][j].sum()
+
+        self.logger.log_t(self.sim.get_transition(), t)
+        #set transition function in pomdp env
+        self.pomdp.pomdpenv.set_transition(t)
+
+    def log_t_complete(self):
+        self.logger.log_t_complete(self.sim.get_transition(), self.pomdp.pomdpenv.get_transition())
+
+    def set_sim_slip(self, pr_succ):
+        #set the transition succes probabilties of the simulation to pr_succ
+        (Tn, Ts, Te, Tw) = get_transitions(pr_succ)
+        nstates = len(self.pomdp.pomdpenv.states)
+        t = np.zeros((self.nactions, nstates, nstates))
+        t[0] = np.array(Tn)
+        t[1] = np.array(Ts)
+        t[2] = np.array(Te)
+        t[3] = np.array(Tw)
+        self.sim.set_transition(t)
+        self.logger.log_pr_succ(pr_succ)
+
+
+    def update_policy(self, mdp_path, pol_path):
+        solve(mdp_path)
+        self.pomdp.pomdppolicy = POMDPPolicy(pol_path)
+
+
 
     def print_summary(self):
         print("---Controller summary---")
@@ -47,6 +93,142 @@ class Controller:
         print("------------------------")
 
 
+class Temporal_controller(Controller):
+    def __init__(self, pomdp, sim, weight, exploration, logger):
+        super().__init__( pomdp, sim, weight, exploration, logger)
+        self.hist_c = [] #clustered history: list of (t_recent_counts, t)
+
+
+    def t_sim_hellinger(self, t1, t2, sums):
+
+        sum = 0
+        for i in range(len(t1)):
+            for j in range(len(t1[i])):
+                bc = 1 - np.sqrt( np.power((np.sqrt(t1[i][j]) - np.sqrt(t2[i][j])), 2).sum())/np.sqrt(2) #1 - helling distance for each action-state pair
+                sum = sum + sums[i][j]*bc
+        print(sum)
+        return sum
+
+    def t_sim(self, t1, t2, sums):
+        sum = 0
+        for i in range(len(t1)):
+            for j in range(len(t1[i])):
+                bc = np.power(np.sqrt(t1[i][j] * t2[i][j]).sum(), 8) #Bhattacharyya coeff for each action-state pair
+                sum = sum + sums[i][j]*bc
+        # print(sum)
+        return sum
+
+
+    def update(self):
+        '''
+        calc most likely transition function t given history
+        t = nparray met elke entry: (a, s, s')
+        '''
+        nstates = len(self.pomdp.pomdpenv.states)
+        t_recent_counts = np.zeros((self.nactions, nstates, nstates))
+
+        b = self.pomdp.belief
+        for step in reversed(self.hist):
+            counts = np.outer(step[0], b) #calc outer product of b(s) and b(s') (product van elke combinatie)
+            t_recent_counts[step[1]] += counts        #add these counts to the corresponding action
+            b = step[0]                   #set b(s') to current for next iter
+
+
+        sums = np.zeros((self.nactions, nstates)) #indicate for each state-action pair how many times the actions was recently taken in the states
+        #normalize
+        t_recent = t_recent_counts.copy()
+        for i in range(len(t_recent)):
+            for j in range(len(t_recent[i])):
+                sum = t_recent[i][j].sum()
+                sums[i][j] = sum
+                if sum == 0: #if unseen set all to one to normalize to random
+                    t_recent[i][j] = np.ones(len(t_recent[i][j]))
+                t_recent[i][j] = t_recent[i][j]/t_recent[i][j].sum()
+
+
+        t = t_recent_counts.copy()*sums.sum()
+
+
+        w = 1
+        for cluster in reversed(self.hist_c):
+            t = t + w * self.t_sim_hellinger(t_recent,cluster[1],sums) * cluster[0]
+            w = w * self.weight
+        #normalize
+        for i in range(len(t)):
+            for j in range(len(t[i])):
+                if t[i][j].sum() == 0: #if unseen set all to one to normalize to random
+                    t[i][j] = np.ones(len(t[i][j]))
+                t[i][j] = t[i][j]/t[i][j].sum()
+
+        self.hist_c.append((t_recent_counts, t)) #of t als laatste param?
+        self.hist = []
+
+
+        self.logger.log_t(self.sim.get_transition(), t)
+        #set transition function in pomdp env
+        self.pomdp.pomdpenv.set_transition(t)
 
 
 
+    def update_alt(self, x):
+        '''
+        calc most likely transition function t given history
+        t = nparray met elke entry: (a, s, s')
+        '''
+        nstates = len(self.pomdp.pomdpenv.states)
+        t_recent_counts = np.zeros((self.nactions, nstates, nstates))
+
+        b = self.pomdp.belief
+        for step in reversed(self.hist):
+            counts = np.outer(step[0], b) #calc outer product of b(s) and b(s') (product van elke combinatie)
+            t_recent_counts[step[1]] += counts        #add these counts to the corresponding action
+            b = step[0]                   #set b(s') to current for next iter
+
+
+        sums = np.zeros((self.nactions, nstates)) #indicate for each state-action pair how many times the actions was recently taken in the states
+        #normalize
+        t_recent = t_recent_counts.copy()
+        for i in range(len(t_recent)):
+            for j in range(len(t_recent[i])):
+                sum = t_recent[i][j].sum()
+                sums[i][j] = sum
+                if sum == 0: #if unseen set all to one to normalize to random
+                    t_recent[i][j] = np.ones(len(t_recent[i][j]))
+                t_recent[i][j] = t_recent[i][j]/t_recent[i][j].sum()
+
+
+        t = t_recent_counts.copy()*sums.sum()
+
+        #alternative impl: first get al sims and only use x most similar
+        w = 1
+        sims = []
+        for cluster in reversed(self.hist_c):
+            sims.append(w * self.t_sim(t_recent,cluster[1],sums))
+            w = w * self.weight
+
+        #flip sims since we looped in reverse
+        sims = np.flip(np.array(sims))
+        #get x highest indices
+        amount = int(np.ceil(x * len(sims)))
+        inds = np.argpartition(sims, -amount)[-amount:]
+       # print("at step: " + str(len(sims)))
+        print(inds)
+        for ind in inds:
+            t = t + sims[ind] * self.hist_c[ind][0]
+
+
+
+        #normalize
+        for i in range(len(t)):
+            for j in range(len(t[i])):
+                if t[i][j].sum() == 0: #if unseen set all to one to normalize to random
+                    t[i][j] = np.ones(len(t[i][j]))
+                t[i][j] = t[i][j]/t[i][j].sum()
+
+        self.hist_c.append((t_recent_counts, t)) #of t als laatste param?
+        self.hist = []
+
+
+        self.logger.log_t(self.sim.get_transition(), t)
+        #set transition function in pomdp env
+        self.pomdp.pomdpenv.set_transition(t)
